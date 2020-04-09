0b826418708606cb8bdcde989d21b203e9949053
Stephanie Boob <stephanie.boob@unic.com>
2020-04-09 18:31:05 +0200

    pushferz

---
+++ src/main/resources/static/js/index.js
@@ -0,0 +1,84 @@
+let root = document.documentElement;
+
+var topText;
+var firstIcon;
+var secondIcon;
+var firstStep;
+var secondStep;
+var middleContent;
+var currentYPosition;
+
+var windowHeight = window.screen.availHeight
+    || document.documentElement.clientHeight
+    || document.body.clientHeight;
+
+var windowWidth = window.screen.availWidth
+    || document.documentElement.clientWidth
+    || document.body.clientWidth;
+
+window.addEventListener('DOMContentLoaded', (event) => {
+    currentYPosition = document.documentElement.scrollTop;
+    window.scrollBy({ top: -currentYPosition, left: 0, behavior: "smooth"});
+
+    topText = document.getElementById("top-text");
+    firstIcon = document.getElementById("firstIcon");
+    secondIcon = document.getElementById("secondIcon");
+    firstStep = document.getElementById("firstStep");
+    secondStep = document.getElementById("secondStep");
+    middleContent = document.getElementById("middleContent");
+    firstIcon.style.background = "radial-gradient(circle, forestgreen 50%, rgba(73, 133, 30, 0) 100%)";
+    secondIcon.style.background = "transparent";
+    secondStep.style.display = "none";
+    document.body.style.setProperty('height', windowHeight + "px");
+    document.body.style.setProperty('width', windowWidth + "px");
+});
+
+
+
+window.onscroll = function () {
+    currentYPosition = document.documentElement.scrollTop;
+    //
+    // if (currentYPosition > 5 && currentYPosition < 15) {
+    //     window.scrollBy({ top: 712, left: 0, behavior: "smooth" });
+    // } else {
+    //     window.scrollBy({ top: -currentYPosition, left: 0, behavior: "smooth" });
+    // }
+    // console.log(currentYPosition);
+    //
+
+    hideTopText(currentYPosition);
+    highlightIcons(currentYPosition);
+}
+
+hideTopText = function (currentPosition) {
+    if (!topText) {
+        return;
+    }
+    if (currentPosition > windowHeight * 0.4) {
+        topText.style.display = "none";
+    } else {
+        topText.style.display = "block";
+    }
+}
+
+highlightIcons = function (currentPosition) {
+    if (!firstIcon || !secondIcon) {
+        return;
+    }
+    if (currentPosition > windowHeight * 0.1) {
+        firstIcon.style.background = "transparent";
+        secondIcon.style.background = "radial-gradient(circle, forestgreen 50%, rgba(73, 133, 30, 0) 100%)";
+        secondStep.style.display = "block";
+    } else {
+        firstIcon.style.background = "radial-gradient(circle, forestgreen 50%, rgba(73, 133, 30, 0) 100%)";
+        secondIcon.style.background = "transparent";
+        secondStep.style.display = "none";
+    }
+
+}
+
+
+
+
+
+
