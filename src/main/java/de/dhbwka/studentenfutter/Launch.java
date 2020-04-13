c28af430cfff81a6ef680631dc7da2225cd53c0a
Alexander Deitel <alex.deitel@googlemail.com>
2020-04-13 12:04:41 +0200

    Moved all files to package de.dhbwka.studentenfutter

---
+++ src/main/java/de/dhbwka/studentenfutter/Launch.java
@@ -0,0 +1,7 @@
+package de.dhbwka.studentenfutter;
+
+public class Launch {
+    public static void main(String[] args) {
+
+    }
+}
--- src/main/java/test.java
+++
@@ -1,8 +0,0 @@
-import org.apache.tomcat.*;
-
-public class test {
-
-    public static void main(String[] args) {
-
-    }
-}
