20d9c63b3274db8038e0fc128ed05fdd6f772015
Alexander Deitel <alex.deitel@googlemail.com>
2020-04-18 18:29:19 +0200

    [BackEnd] Adjusted Servlet Mapping via @WebServlet annotation

--- src/main/java/de/dhbwka/studentenfutter/servlets/IndexServlet.java
+++ src/main/java/de/dhbwka/studentenfutter/servlets/IndexServlet.java
@@ -1,11 +1,13 @@
 package de.dhbwka.studentenfutter.servlets;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@WebServlet("/index")
 public class IndexServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
     }
