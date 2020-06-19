357f27bc6a5ab6bcbba26e752381994a0bb74f78
Kai Hatt <kai.hatt12@gmail.com>
2020-06-19 15:12:07 +0200

    [FrontEnd] Added buttonPrintShoppingList functionality

--- src/main/webapp/js/shopping_list.js
+++ src/main/webapp/js/shopping_list.js
@@ -207,7 +207,17 @@ function buildShoppingListTable(shoppingListObject) {
 }
 
 function printShoppingList() {
+        var printWindow = window.open();
 
+        printWindow.document.write('<html><head><title>' + document.title  + '</title>');
+        printWindow.document.write('</head><body >');
+        printWindow.document.write('<h1>' + document.title  + '</h1>');
+        printWindow.document.write(document.getElementById("toPrint").innerHTML);
+        printWindow.document.write('</body></html>');
 
+        printWindow.document.close(); // necessary for IE >= 10
+        printWindow.focus(); // necessary for IE >= 10
 
+        printWindow.print();
+        printWindow.close();
 }
