e8cae5e9186fe065b16782c45adc26060c7715c4
Alexander Deitel <alex.deitel@googlemail.com>
2020-06-21 05:48:17 +0200

    Simple img data retrieving test

--- src/main/java/de/dhbwka/studentenfutter/servlets/IndexServlet.java
+++ src/main/java/de/dhbwka/studentenfutter/servlets/IndexServlet.java
@@ -1,22 +1,29 @@
 package de.dhbwka.studentenfutter.servlets;
 
+import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 
 @WebServlet(urlPatterns = "/index")
+@MultipartConfig //need for retrieving multipart/form-data
 public class IndexServlet extends AbstractServlet {
+
+    @Override
+    protected void handleDoPost(HttpServletRequest req, HttpServletResponse res) throws Exception {
+        System.out.println(req.getPart("img").getInputStream().readAllBytes().length);
+    }
 
     @Override
     protected void handleDoGet(HttpServletRequest req, HttpServletResponse res) throws Exception {
         var userId = "1"; //req.getParameter("userId");
 
-       var name =
-        getDataAccess().query("select name from user where id_user=?")
-                .withParam(userId).runAs(String.class).get().get();
-       req.getSession().setAttribute("username", name);
+        var name =
+                getDataAccess().query("select name from user where id_user=?")
+                        .withParam(userId).runAs(String.class).get().get();
+        req.getSession().setAttribute("username", name);
 
-       req.getRequestDispatcher("/jsp/index.jsp").forward(req, res);
+        req.getRequestDispatcher("/jsp/index.jsp").forward(req, res);
     }
 }
