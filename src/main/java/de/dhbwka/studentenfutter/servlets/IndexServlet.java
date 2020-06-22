8db63d6a9a81f44093180f9860cf0c96bad61a1f
Alexander Deitel <alex.deitel@googlemail.com>
2020-06-22 09:03:55 +0200

    Added default IndexServlet routing

--- src/main/java/de/dhbwka/studentenfutter/servlets/IndexServlet.java
+++ src/main/java/de/dhbwka/studentenfutter/servlets/IndexServlet.java
@@ -6,18 +6,20 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 
-@WebServlet(urlPatterns = "/index")
+@WebServlet(urlPatterns = {"/", "/index"})
 @MultipartConfig //need for retrieving multipart/form-data
 public class IndexServlet extends AbstractServlet {
 
     @Override
     protected void handleDoPost(HttpServletRequest req, HttpServletResponse res) throws Exception {
-        System.out.println(req.getPart("img").getInputStream().readAllBytes().length);
+        System.out.println(req.getPart("img").getContentType()); //getHeader("content-disposition")); //get filename in javax 3.0
+        res.reset();
     }
 
     @Override
     protected void handleDoGet(HttpServletRequest req, HttpServletResponse res) throws Exception {
         var userId = "1"; //req.getParameter("userId");
+
 
         var name =
                 getDataAccess().query("select name from user where id_user=?")
