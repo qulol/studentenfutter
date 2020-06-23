f3989ee7d04ba2e972cda93b3e124f028eae49c3
Alexander Deitel <alex.deitel@googlemail.com>
2020-06-23 09:08:23 +0200

    Fixed error IndexServlet.java

--- src/main/java/de/dhbwka/studentenfutter/servlets/IndexServlet.java
+++ src/main/java/de/dhbwka/studentenfutter/servlets/IndexServlet.java
@@ -18,15 +18,6 @@ public class IndexServlet extends AbstractServlet {
 
     @Override
     protected void handleDoGet(HttpServletRequest req, HttpServletResponse res) throws Exception {
-        var session = req.getSession();
-        var userId = session.getAttribute("userId");
-
-        var name =
-                getDataAccess().query("select name from user where id_user=?")
-                        .withParam(userId).collectAs(String.class).get().get();
-
-        session.setAttribute("username", name);
-
         req.getRequestDispatcher("/jsp/index.jsp").forward(req, res);
     }
 }
