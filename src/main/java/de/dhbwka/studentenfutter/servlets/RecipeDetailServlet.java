8b5a6b34d3e269979cbd6caa7a5a5b72e24846fb
Alexander Deitel <alex.deitel@googlemail.com>
2020-06-27 13:04:31 +0200

    Implemented RecipeDetailServlet

--- src/main/java/de/dhbwka/studentenfutter/servlets/RecipeDetailServlet.java
+++ src/main/java/de/dhbwka/studentenfutter/servlets/RecipeDetailServlet.java
@@ -1,16 +1,47 @@
 package de.dhbwka.studentenfutter.servlets;
 
-import javax.servlet.ServletException;
+import de.dhbwka.studentenfutter.bean.DescriptionBean;
+import de.dhbwka.studentenfutter.bean.IngredientBean;
+import de.dhbwka.studentenfutter.bean.RecipeBean;
+
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 @WebServlet(urlPatterns = "/recipedetail")
 public class RecipeDetailServlet extends AbstractServlet {
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+    protected void handleDoGet(HttpServletRequest req, HttpServletResponse res) throws Exception {
+        var id = req.getParameter("id");
+
+        var db = getDataAccess();
+        var optionalRecipeBean = db
+                .cachedQuery("sql/selectRecipeModel.sql")
+                .withParam(id)
+                .collectAs(RecipeBean.class)
+                .get();
+
+        if (optionalRecipeBean.isEmpty()) {
+           throw new Exception();
+        }
+
+        var ingredientBean = db
+                .cachedQuery("sql/selectRecipeIngredient.sql")
+                .withParam(id)
+                .collectAs(IngredientBean.class)
+                .getList();
+        var descriptionBean = db
+                .cachedQuery("")
+                .withParam(id)
+                .collectAs(DescriptionBean.class)
+                .getList();
+
+        var recipeBean = optionalRecipeBean.get();
+        recipeBean.setIngredients(ingredientBean);
+        recipeBean.setDescriptions(descriptionBean);
+
+        req.setAttribute("recipe", recipeBean);
         req.getRequestDispatcher(req.getContextPath().concat("/jsp/recipe_detail.jsp")).forward(req, res);
     }
 }
