988eefb628e4f70b99fa2c13d8369d7583c81031
Alexander Deitel <alex.deitel@googlemail.com>
2020-06-27 12:25:01 +0200

    reformat

--- src/main/resources/sql/insertRecipe.sql
+++ src/main/resources/sql/insertRecipe.sql
@@ -1,3 +1,3 @@
 insert into recipe (id_user, name, category, description_0, description_1, description_2, description_3, description_4,
                     description_5, description_6, description_7, description_8, description_9)
-                    values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
