3cd39c353377452396fc94c3ebd4e0b3ec213738
Kai Hatt <kai.hatt12@gmail.com>
2020-06-28 10:11:28 +0200

    Add IngredientsListBean with all ingredients to session when ShoppingCartAddToServlet is called

--- src/main/java/de/dhbwka/studentenfutter/servlets/ShoppingCartServlet.java
+++ src/main/java/de/dhbwka/studentenfutter/servlets/ShoppingCartServlet.java
@@ -1,5 +1,8 @@
 package de.dhbwka.studentenfutter.servlets;
 
+
+import de.dhbwka.studentenfutter.bean.IngredientsListBean;
+
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -9,6 +12,8 @@ public class ShoppingCartServlet extends AbstractServlet {
 
     @Override
     protected void handleDoGet(HttpServletRequest req, HttpServletResponse res) throws Exception {
+        IngredientsListBean ingredients = (IngredientsListBean) req.getSession().getAttribute("ingredientsList");
+
         req.getRequestDispatcher("/jsp/shoppingcart.jsp").forward(req, res);
     }
 }
