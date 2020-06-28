4a830668bbfee141ebe1971a4f25bec3459c21d8
Alexander Deitel <alex.deitel@googlemail.com>
2020-06-28 12:18:47 +0200

    Revert "Add IngredientsListBean with all ingredients to session when ShoppingCartAddToServlet is called"
    This reverts commit 3cd39c35

--- src/main/java/de/dhbwka/studentenfutter/servlets/ShoppingCartAddToServlet.java
+++ src/main/java/de/dhbwka/studentenfutter/servlets/ShoppingCartAddToServlet.java
@@ -1,12 +1,8 @@
 package de.dhbwka.studentenfutter.servlets;
 
-import de.dhbwka.studentenfutter.bean.IngredientBean;
-import de.dhbwka.studentenfutter.bean.IngredientsListBean;
-
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.List;
 
 @WebServlet(urlPatterns = "/addtoshoppingcard")
 public class ShoppingCartAddToServlet extends AbstractServlet {
@@ -14,19 +10,5 @@ public class ShoppingCartAddToServlet extends AbstractServlet {
     @Override
     protected void handleDoPost(HttpServletRequest req, HttpServletResponse res) throws Exception {
         res.sendRedirect("/recipedetail?id=".concat(req.getParameter("id").concat("#ingredientsTab")));
-
-        var recipeId = req.getParameter("id");
-
-        var db = getDataAccess();
-
-        List<IngredientBean> ingredients = db
-                .cachedQuery("sql/selectRecipeIngredient")
-                .withParam(recipeId)
-                .collectAs(IngredientBean.class)
-                .getList();
-
-        IngredientsListBean ingredientsListBean = new IngredientsListBean(ingredients);
-
-        req.getSession().setAttribute("ingredientsList", ingredientsListBean);
     }
 }
--- src/main/java/de/dhbwka/studentenfutter/servlets/ShoppingCartServlet.java
+++ src/main/java/de/dhbwka/studentenfutter/servlets/ShoppingCartServlet.java
@@ -1,8 +1,5 @@
 package de.dhbwka.studentenfutter.servlets;
 
-
-import de.dhbwka.studentenfutter.bean.IngredientsListBean;
-
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -12,8 +9,6 @@ public class ShoppingCartServlet extends AbstractServlet {
 
     @Override
     protected void handleDoGet(HttpServletRequest req, HttpServletResponse res) throws Exception {
-        IngredientsListBean ingredients = (IngredientsListBean) req.getSession().getAttribute("ingredientsList");
-
         req.getRequestDispatcher("/jsp/shoppingcart.jsp").forward(req, res);
     }
 }
