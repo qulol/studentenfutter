f289ec48c788b7cdbb459b7aa0caa14d49beb86b
Alexander Deitel <alex.deitel@googlemail.com>
2020-06-28 14:47:25 +0200

    added new fields to user bean

--- src/main/java/de/dhbwka/studentenfutter/bean/UserBean.java
+++ src/main/java/de/dhbwka/studentenfutter/bean/UserBean.java
@@ -1,13 +1,15 @@
 package de.dhbwka.studentenfutter.bean;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 public class UserBean implements Serializable {
-    private final boolean loggedIn;
-    private final String username;
+    private boolean loggedIn;
+    private String username;
+    private List<IngredientBean> shoppingCard;
 
     public UserBean() {
-        this(false, "unknown");
     }
 
     public UserBean(String username) {
@@ -19,11 +21,30 @@ public class UserBean implements Serializable {
         this.username = username;
     }
 
+    public List<IngredientBean> getShoppingCard() {
+        if (shoppingCard == null) {
+            shoppingCard = new ArrayList<>();
+        }
+        return shoppingCard;
+    }
+
+    public void setShoppingCard(List<IngredientBean> shoppingCard) {
+        this.shoppingCard = shoppingCard;
+    }
+
+    public void setLoggedIn(boolean loggedIn) {
+        this.loggedIn = loggedIn;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
     public boolean isLoggedIn() {
         return loggedIn;
     }
 
     public String getUsername() {
-        return username;
+        return username == null ? "anonym" : username;
     }
 }
