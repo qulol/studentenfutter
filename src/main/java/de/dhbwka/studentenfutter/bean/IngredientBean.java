13701cb193ad3cd2f6d59dab5c076d73d3f5066a
Alexander Deitel <alex.deitel@googlemail.com>
2020-06-28 14:47:56 +0200

    added simple logic operations to IngredientBean

--- src/main/java/de/dhbwka/studentenfutter/bean/IngredientBean.java
+++ src/main/java/de/dhbwka/studentenfutter/bean/IngredientBean.java
@@ -1,17 +1,38 @@
 package de.dhbwka.studentenfutter.bean;
 
 import de.dhbwka.studentenfutter.database.query.QueryResult;
+import de.dhbwka.studentenfutter.util.TypeMissMatchException;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 public class IngredientBean implements Serializable {
-    @QueryResult(column = "amount")
-    private float amount;
-    @QueryResult(column = "unit")
-    private String unit;
     @QueryResult(column = "ingredient")
     private String name;
+    @QueryResult(column = "unit")
+    private String unit;
+    @QueryResult(column = "amount")
+    private float amount;
 
+    public IngredientBean() {
+    }
+
+    public IngredientBean(String name, String unit, float amount) {
+        this.name = name;
+        this.unit = unit;
+        this.amount = amount;
+    }
+
+    public IngredientBean multiply(float factor) {
+        return new IngredientBean(name, unit, amount * factor);
+    }
+
+    public IngredientBean add(IngredientBean other) throws TypeMissMatchException {
+        if(!this.equals(other)) {
+            throw new TypeMissMatchException();
+        }
+        return new IngredientBean(name, unit, amount +  other.amount);
+    }
 
     public float getAmount() {
         return amount;
@@ -23,5 +44,19 @@ public class IngredientBean implements Serializable {
 
     public String getName() {
         return name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        IngredientBean that = (IngredientBean) o;
+        return name.equals(that.name) &&
+                unit.equals(that.unit);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, unit);
     }
 }
