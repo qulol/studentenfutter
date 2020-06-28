effff67ae365c6162fd17cbec54ef84aad6b8720
Alexander Deitel <alex.deitel@googlemail.com>
2020-06-28 08:25:07 +0200

    delete unused fields in recipes_detail.js

--- src/main/webapp/js/recipes_detail.js
+++ src/main/webapp/js/recipes_detail.js
@@ -1,23 +1,11 @@
 
 var numberOfPersonsReference = 1;
-var ingredientsTableReference;
-var instructionsListReference;
-var headlineReference;
-var imageReference;
 
 window.addEventListener('DOMContentLoaded', () => {
-    ingredientsTableReference = document.getElementById("ingredientsTable");
-    instructionsListReference = document.getElementById("instructions");
-    headlineReference = document.getElementById("headline");
-    imageReference = document.getElementById("recipe-image-detail")
-
     numberOfPersonsReference = document.getElementById("numberOfPersons");
-    headlineReference.innerHTML = mealObject.headline;
-    imageReference.src = mealObject.image;
 });
 
 function calculateAmount() {
-
     for (var i = 0; i < mealObject.ingredients.length; i++) {
         var numberOfPersonsValue = numberOfPersonsReference.value;
         var amountReference = document.getElementById("amount" + i);
