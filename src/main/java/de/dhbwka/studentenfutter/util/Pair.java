0e924189ce82e89d0eec98106bcfe184791b0f9a
Alexander Deitel <alex.deitel@googlemail.com>
2020-06-28 14:46:37 +0200

    new util class for a simple value pair

---
+++ src/main/java/de/dhbwka/studentenfutter/util/Pair.java
@@ -0,0 +1,35 @@
+package de.dhbwka.studentenfutter.util;
+
+import java.util.Objects;
+
+public class Pair<T, S> {
+    private final T first;
+    private final S second;
+
+    public Pair(T first, S second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public T getFirst() {
+        return first;
+    }
+
+    public S getSecond() {
+        return second;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Pair<?, ?> pair = (Pair<?, ?>) o;
+        return Objects.equals(first, pair.first) &&
+                Objects.equals(second, pair.second);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(first, second);
+    }
+}
