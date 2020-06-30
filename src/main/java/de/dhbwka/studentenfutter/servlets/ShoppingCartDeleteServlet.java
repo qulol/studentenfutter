5e9e636a62425a525f0b1608f9853bd807f5e417
Alexander Deitel <alex.deitel@googlemail.com>
2020-06-30 17:56:18 +0200

    added ShoppingCartDeleteServlet.java

---
+++ src/main/java/de/dhbwka/studentenfutter/servlets/ShoppingCartDeleteServlet.java
@@ -0,0 +1,17 @@
+package de.dhbwka.studentenfutter.servlets;
+
+import de.dhbwka.studentenfutter.bean.UserBean;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = "/deleteshoppingcart")
+public class ShoppingCartDeleteServlet extends AbstractServlet {
+
+    @Override
+    protected void handleDoGet(HttpServletRequest req, HttpServletResponse res) throws Exception {
+        var shoppingCart = ((UserBean)req.getSession().getAttribute("user"));
+        shoppingCart.getShoppingCard().clear();
+    }
+}
