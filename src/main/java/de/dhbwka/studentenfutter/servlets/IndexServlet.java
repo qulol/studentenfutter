77340a76994971e65c849b8c45074dc33da0aad2
Alexander Deitel <alex.deitel@googlemail.com>
2020-07-01 09:09:05 +0200

    cleanup

--- src/main/java/de/dhbwka/studentenfutter/servlets/IndexServlet.java
+++ src/main/java/de/dhbwka/studentenfutter/servlets/IndexServlet.java
@@ -7,14 +7,7 @@ import javax.servlet.http.HttpServletResponse;
 
 
 @WebServlet(urlPatterns = "/index")
-@MultipartConfig //need for retrieving multipart/form-data
 public class IndexServlet extends AbstractServlet {
-
-    @Override
-    protected void handleDoPost(HttpServletRequest req, HttpServletResponse res) throws Exception {
-        System.out.println(req.getPart("img").getContentType()); //getHeader("content-disposition")); //get filename in javax 3.0
-        res.reset();
-    }
 
     @Override
     protected void handleDoGet(HttpServletRequest req, HttpServletResponse res) throws Exception {
