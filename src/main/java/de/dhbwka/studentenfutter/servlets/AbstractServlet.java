ccb7b001e004bc4756c5b040bc7ddfdaefc7127e
Alexander Deitel <alex.deitel@googlemail.com>
2020-07-17 01:41:16 +0200

    cleanup doc changes

--- src/main/java/de/dhbwka/studentenfutter/servlets/AbstractServlet.java
+++ src/main/java/de/dhbwka/studentenfutter/servlets/AbstractServlet.java
@@ -4,7 +4,6 @@ import de.dhbwka.studentenfutter.AppInitializer;
 import de.dhbwka.studentenfutter.database.DatabaseAccess;
 import de.dhbwka.studentenfutter.util.ServletMethodNotImplementedException;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
